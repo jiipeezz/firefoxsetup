e5766317554879c25a6e983143a1975e57c1f57b
jiipeezz <a1500947@myy.haaga-helia.fi>
2016-12-14 09:01:19 +0200

    Update syspref.js

--- syspref.js
+++ syspref.js
@@ -1,4 +1,4 @@
-LockPref("browser.startup.homepage", "https://www.usa.gov");
+lockPref("browser.startup.homepage", "https://www.usa.gov");
 user_pref("browser.search.countryCode", "US");
 user_pref("browser.search.region", "US");
 user_pref("signon.rememberSignons", false);
